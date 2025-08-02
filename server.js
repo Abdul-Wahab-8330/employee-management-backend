7f836329320d0f989b935eec7cad2ed31f56736e
Abdul Wahab <abdulwahabee5@gmail.com>
2025-08-02 11:31:40 +0500

    Update server.js

--- server.js
+++ server.js
@@ -12,11 +12,7 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors({
-  origin: "https://xtech-employee-management-system.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json())
 
 
